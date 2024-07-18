309b1e25c54e98754062439a7c144da4404f9dcc
Iglesia Dolci <daia.dolci@gmail.com>
2024-07-18 13:51:24 +0100

    wip

--- pyadjoint/adjfloat.py
+++ pyadjoint/adjfloat.py
@@ -132,7 +132,7 @@ class AdjFloat(OverloadedType, float):
         NotImplementedError("It requires more thought to return a PETSc Vec from `AdjFloat`")
 
     def _ad_vec_from_petsc(self, vec):
-        NotImplementedError("It requires more thought to return a PETSc Vec from `AdjFloat`")
+        NotImplementedError("It requires more thought.")
 
 
 _min = min
